e6af4b6d5777fac8b0a5b7298308a1fbd36e39f0
Panji Arlin Saputra <75957797+panjiarlins@users.noreply.github.com>
2023-09-16 16:40:43 +0700

    fix: verifyUserAuth

--- src/middlewares/auth/verifyUserAuth.js
+++ src/middlewares/auth/verifyUserAuth.js
@@ -3,24 +3,25 @@ const { ResponseError } = require('../../errors');
 const { User } = require('../../models');
 const sendResponse = require('../../utils/sendResponse');
 
-function verifyUserAuth({ isAdmin = false, isCashier = false }) {
-  return (req, res, next) => {
+function verifyUserAuth({ isAdmin = false, isCashier = false } = {}) {
+  return async (req, res, next) => {
     try {
-      jwt.verify(req.token, process.env.JWT_SECRET_KEY, (err, decoded) => {
-        if (err) throw new ResponseError(err, 401);
+      await jwt.verify(
+        req.token,
+        process.env.JWT_SECRET_KEY,
+        async (err, decoded) => {
+          if (err) throw new ResponseError(err, 401);
 
-        User.findByPk(decoded.id, {
-          attributes: ['isAdmin', 'isCashier'],
-        })
-          .then((userData) => {
-            if (isAdmin && userData.isAdmin) return;
-            if (isCashier && userData.isCashier) return;
-            throw new ResponseError('user unauthorized', 401);
-          })
-          .catch((error) => {
-            throw error;
+          const userData = await User.findByPk(decoded.id, {
+            attributes: ['isAdmin', 'isCashier'],
           });
-      });
+
+          if (!userData) throw new ResponseError('user unauthorized', 401);
+          if (isAdmin && userData.isAdmin) return;
+          if (isCashier && userData.isCashier) return;
+          throw new ResponseError('user unauthorized', 401);
+        }
+      );
 
       next();
     } catch (error) {
